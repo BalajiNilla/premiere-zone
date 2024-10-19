0b69538e000ba01db4a1f5208ef3257723ce896d
balaji nilla <nillabalaji@gmail.com>
2024-10-19 13:46:04 +0530

    creating the project

---
+++ src/test/java/com/pl/primere_zone/PrimereZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.primere_zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimereZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
